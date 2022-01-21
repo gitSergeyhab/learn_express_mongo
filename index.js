1aea5c0a509135f65c681f5e8b87213a4c0bf656
Sergey <redmoloch@yandex.ru>
2022-01-21 22:00:31 +0300

    added user actions, split router

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const fs = require('fs')
 const express = require('express');
+const { use } = require('express/lib/application');
 
-const PATH_TOUR_SIMPLE = '/dev-data/data/tours-simple.json';
-const PATH_TOUR = '/dev-data/data/tours.json';
+const Path = {
+    TourSimple: '/dev-data/data/tours-simple.json',
+    User: '/dev-data/data/users.json',
+    Tour: '/dev-data/data/tours.json'
+};
 
 const ApiRoute = {
     Main: '/',
     Tours:  '/api/v1/tours',
-}
+    Users: '/api/v1/users'
+};
 
 
 const port = 3000;
@@ -21,8 +26,16 @@ app.use((req, res, next) => {
 })
 
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}${PATH_TOUR_SIMPLE}`));
+const tours = JSON.parse(fs.readFileSync(`${__dirname}${Path.TourSimple}`));
+const users = JSON.parse(fs.readFileSync(`${__dirname}${Path.User}`));
 
+const tourRouter = express.Router();
+const userRouter = express.Router();
+
+app.use(ApiRoute.Tours, tourRouter);
+app.use(ApiRoute.Users, userRouter);
+
+// TOURS
 const getAllTours = (req, res) => {
     res
         .status(200)
@@ -51,18 +64,6 @@ const getTour = (req, res) => {
         });
 };
 
-const postTour = (req, res) => {
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = {...req.body, id: newId};
-    const newTours = [...tours, newTour];
-    fs.writeFile(`${__dirname}${PATH_TOUR_SIMPLE}`, JSON.stringify(newTours), (err) => {
-        res.status(201).json({
-            status: 'success',
-            data: {tour: newTour},
-        })
-    })
-};
-
 const patchTour = (req, res) => {
     const id = +req.params.id;
     const tour = tours.find((item) => item.id === id);
@@ -81,6 +82,20 @@ const patchTour = (req, res) => {
         });
 };
 
+const postTour = (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = {...req.body, id: newId};
+    const newTours = [...tours, newTour];
+    fs.writeFile(`${__dirname}${Path.TourSimple}`, JSON.stringify(newTours), (err) => {
+        res.status(201).json({
+            status: 'success',
+            data: {tour: newTour},
+        })
+    })
+};
+
+
+
 const deleteTour = (req, res) => {
     const id = +req.params.id;
     const tour = tours.find((item) => item.id === id);
@@ -98,11 +113,88 @@ const deleteTour = (req, res) => {
         });
 };
 
-app.route(ApiRoute.Tours)
+
+// USERS
+
+const getAllUsers = (req, res) => {
+    res
+        .status(200)
+        .json({
+            time: req.requestTime,
+            status: 'success',
+            length: users.length,
+            data: {users},
+        });
+}
+
+const getUser = (req, res) => {
+    const id = req.params.id;
+    const user = users.find((item) => item._id === id);
+    if (!user) {
+        return res.status(404).json({
+            status: 'ERROR',
+            message: `there is no user with ID :  ${id}`
+        })
+    }
+    res
+        .status(200)
+        .json({
+            status: 'success',
+            data: {user},
+        });
+};
+
+const postUser = (req, res) => {
+    const newId = users[users.length - 1].id + '1';
+    const newUser = {...req.body, id: newId};
+    const newUsers = [...users, newUser];
+    fs.writeFile(`${__dirname}${Path.User}`, JSON.stringify(newUsers), (err) => {
+        res.status(201).json({
+            status: 'success',
+            data: {user: newUser},
+        })
+    })
+};
+
+const patchUser = (req, res) => {
+    const id = req.params.id;
+    const user = users.find((item) => item._id === id);
+    if (!user) {
+        return res.status(404).json({
+            status: 'ERROR',
+            message: `there is no user with ID :  ${id}`
+        })
+    }
+    res
+        .status(200)
+        .json({
+            status: 'success',
+            data: {user},
+        });
+};
+
+const deleteUser = (req, res) => {
+    const id = req.params.id;
+    const user = users.find((item) => item._id === id);
+    if (!user) {
+        return res.status(404).json({
+            status: 'ERROR',
+            message: `there is no user with ID :  ${id}`
+        })
+    }
+    res
+        .status(204)
+        .json({
+            status: 'success',
+            data: {delete: true},
+        });
+};
+
+tourRouter.route('/')
     .get(getAllTours)
     .post(postTour)
 
-app.route(`${ApiRoute.Tours}/:id`)
+tourRouter.route('/:id')
     .get(getTour)
     .patch(patchTour)
     .delete(deleteTour)
@@ -112,6 +204,14 @@ app.route(`${ApiRoute.Tours}/:id`)
 // app.post(ApiRoute.Tours, postTour);
 // app.patch(`${ApiRoute.Tours}/:id`, patchTour);
 // app.delete(`${ApiRoute.Tours}/:id`, deleteTour);
+userRouter.route('/')
+    .get(getAllUsers)
+    .post(postUser)
+
+userRouter.route('/:id')
+    .get(getUser)
+    .patch(patchUser)
+    .delete(deleteUser)
 
 app.listen(port, () => {
     console.log(`App listen port ${port}`)
