49ee6ec5019e5421e605eef97882469e81a6934d
Sergey <redmoloch@yandex.ru>
2022-01-28 13:13:31 +0300

    added bcryptjs for passwords

--- utils/app-error.js
+++ utils/app-error.js
@@ -4,8 +4,6 @@ class AppError extends Error {
     this.statusCode = statusCode;
     this.status = statusCode.toString().startsWith('4') ? 'fail' : 'error';
     this.isOperational = true;
-    console.log(this);
-
     Error.captureStackTrace(this, this.constructor);
   }
 }
